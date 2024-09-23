ec63bd428d15ef29f49b8b8424a84622f8248326
Hunter Smith <huntertsmith4778@gmail.com>
2024-09-22 22:42:06 -0400

    cmake build

---
+++ build/googletest/googlemock/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/Break/OneDrive - Virginia State University/Classes/Fall24/Programming Languages CSCI 485/MostOccuringChar/googletest/googlemock
+# Build directory: C:/Users/Break/OneDrive - Virginia State University/Classes/Fall24/Programming Languages CSCI 485/MostOccuringChar/build/googletest/googlemock
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("../googletest")
